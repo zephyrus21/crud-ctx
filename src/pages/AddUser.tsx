328d39563f11a5d9b6438298195413ff90f03dee
zephyrus21 <pandey.piyus@gmail.com>
2021-08-24 02:05:50 +0530

    adding new user

--- src/pages/AddUser.tsx
+++ src/pages/AddUser.tsx
@@ -1,15 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { addUser } from "../context/action";
+import { GlobalContext } from "../context/GlobalState";
+
 
 interface AddUserProps {}
 
 const AddUser: React.FC<AddUserProps> = ({}) => {
+  const history = useHistory();
+  const { dispatch } = useContext(GlobalContext);
+  const [name, setName] = useState("");
+
+  const onSubmitHandler = () => {
+    const newUser = {
+      id: ,
+      name: name,
+    };
+    dispatch(addUser(newUser));
+    history.push("/");
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmitHandler}>
       <FormGroup>
         <Label>Name</Label>
-        <Input type='text' placeholder='Name...'></Input>
+        <Input
+          type='text'
+          placeholder='Name...'
+          value={name}
+          onChange={(e) => setName(e.target.value)}></Input>
       </FormGroup>
       <Button type='submit'>Submit</Button>
       <Link to='/' className='btn btn-danger ml-2'>
